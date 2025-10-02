08444ed736e3bc6cf0ee5e61524a4a88a953cdda
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-10-02 15:46:43 +0600

    blog posts

--- functions.php
+++ functions.php
@@ -8,7 +8,7 @@ add_action("after_setup_theme", "alpha_bootstrapping");
 
 function alpha_assets(){
     wp_enqueue_style("alpha", get_stylesheet_uri());
-    wp_enqueue_style("bootstrap", "//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
+    wp_enqueue_style("bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css");
 }
 
 add_action("wp_enqueue_scripts", "alpha_assets");
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <?php wp_head(); ?>
 </head>
-<body>
+<body <?php body_class(); ?>>
    <div class="header">
         <div class="container">
         <div class="row">
@@ -16,7 +16,53 @@
                 <h1 class="align-self-center display-1 text-center heading">
                     <?php bloginfo("name"); ?>
                 </h1>
+            </div>
+        </div>
+        </div>    
     
+   </div>
+   <div class="posts">
+    <?php 
+    while(have_posts()){
+        the_post();
+        ?>
+
+        <div class="post" <?php post_class(); ?>>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2 class="post-title"><?php the_title();?></h2>
+            </div> 
+            
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <p>
+                    <strong> <?php the_author();?></strong><br/>
+                    <?php the_date();?>
+                </p>
+                <ul class="list-unstyled">
+                    <li>dhaka</li>
+                </ul>    
+            </div>
+            <div class="col-md-8">
+                <p> 
+                    <?php
+                    if(has_post_thumbnail()){
+                        the_post_thumbnail("large", array("class='img-fluid'"));
+                    }
+                    ?>
+
+                </p>
+                <?php the_content(); ?>
+            </div>
+        </div>
+        </div>
+        </div>
+        <?php
+    }  
+    ?>
+
    </div>
 <?php wp_footer(); ?>   
 </body>
